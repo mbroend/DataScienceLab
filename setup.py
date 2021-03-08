09267c9a1f6ffc6eb507dd66870c47b367aadf0f
mabrsr <mabrsr@rm.dk>
2021-03-08 16:46:36 +0100

    third commit

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
   install_requires=[            # I get to this in a second
           'sqlalchemy',
           'pandas',
-          'urllib',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
