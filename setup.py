0b959882bad18a7220f2fbb015e4aaf5f9b0ce21
Mathias Brønd Sørensen <mabrsr@rm.dk>
2021-10-08 03:50:07 +0200

    Added additional metadata about the package

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 
 from distutils.core import setup
+import DataScienceLab as dsl
+ver = dsl.__version__
+
 setup(
   name = 'DataScienceLab',         # How you named your package folder (MyLib)
   packages = ['DataScienceLab'],   # Chose the same as "name"
-  version = '0.1.6',      # Start with a small number and increase it with every change you make
+  version = ver,      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Building blocks for common data science work',   # Give a short description about your library
   author = 'Mathias Brønd Sørensen',                   # Type in your name
   author_email = 'mathias.b.sorensen@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mbroend/DataScienceLab/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mbroend/DataScienceLab/archive/v_01.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mbroend/DataScienceLab/archive/v_'+ver[0]+ver[2:]+'.tar.gz',    # release URL
   keywords = ['data science', 'SQL', 'SERVER', 'CONNECTION', 'SIMPLIFIED', 'machine learning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyodbc',
